e91737ce954a8a6cc83d8fbcb26896577ce505a0
skoflok <skoflok@gmail.com>
2022-02-23 12:49:17 +0400

    Fixed. solution.go

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/skoflok/golang-united-school-homework-2
+
+go 1.15
--- solution.go
+++ solution.go
@@ -1,5 +1,7 @@
 package square
 
+import "math"
+
 // Define custom int type to hold sides number and update CalcSquare signature by replacing #yourTypeNameHere#
 
 // Define constants to represent 0, 3 and 4 sides.  Test uses mnemos: SidesTriangle(==3), SidesSquare(==4), SidesCircle(==0)
@@ -8,5 +10,44 @@ package square
 // CalcSquare(10.0, SidesSquare)
 // CalcSquare(10.0, SidesCircle)
 
-func CalcSquare(sideLen float64, sidesNum #yourTypeNameHere#) float64 {
+
+type SideCounter int
+
+const Pi float = math.Pi
+const SidesTriangle int = 3
+const SidesSquare int = 4
+const SidesCircle int = 0
+
+var availableSides = []int{SidesTriangle, SidesSquare, SidesCircle}
+
+func CalcSquare(sideLen float64, sidesNum SideCounter) float64 {
+	var square float64 = 0
+	switch sidesNum {
+	case 0:
+		square = calcCircle(sideLen)
+		break
+	case 3:
+		square = caclTriangle(sideLen)
+		break
+	case 4:
+		square = calcSquare(sideLen)
+	default:
+		square = 0
+	}
+	return square
+}
+
+func caclTriangle(sideLen float64) float64
+{
+	const float64 coef = Pow(3, 1/2)/4
+	return coef * Pow(sideLen,2)
+}
+
+func calcSquare(sideLen float64) float64
+{
+	return pow(sideLen, 2)
+}
+
+func calcCircle(sideLen float64) float64 {
+	return Pi * Pow(sideLen,2)
 }
